189779890ccbf5d48a1063afd557004be3e71b16
Eddy Briand <333ebr@gmail.com>
2023-06-01 18:09:48 +0200

    feat: google signin

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from '../home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomePageComponent,
+    component: LoginPageComponent,
   },
   {
     path: 'login',
