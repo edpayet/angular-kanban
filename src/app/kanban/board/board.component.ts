de5c2d23d09998d8dd6d9963750d1b4b0c36964d
Eddy Briand <333ebr@gmail.com>
2023-06-16 01:54:53 +0200

    feat: kanban CRUD on board and tasks

--- src/app/kanban/board/board.component.ts
+++ src/app/kanban/board/board.component.ts
@@ -2,6 +2,9 @@ import { Component, Input } from '@angular/core';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { BoardService } from '../board.service';
 import { Board } from '../board.model';
+import { Task } from '../board.model';
+import { MatDialog } from '@angular/material/dialog';
+import { TaskDialogComponent } from '../dialogs/task-dialog.component';
 
 @Component({
   selector: 'app-board',
@@ -11,7 +14,7 @@ import { Board } from '../board.model';
 export class BoardComponent {
   @Input() board!: Board;
 
-  constructor(private boardService: BoardService) {}
+  constructor(private boardService: BoardService, private dialog: MatDialog) {}
 
   taskDrop(event: CdkDragDrop<string[]>) {
     if (this.board.tasks && this.board.id) {
@@ -22,5 +25,36 @@ export class BoardComponent {
       );
       this.boardService.updateTasks(this.board.id, this.board.tasks);
     }
+  }
+
+  openDialog(task?: Task, idx?: number): void {
+    const newTask = { label: 'purple' };
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '500px',
+      data: task
+        ? { task: { ...task }, isNew: false, boardId: this.board.id, idx }
+        : { task: newTask, isNew: true },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        if (this.board.id && this.board.tasks) {
+          if (result.isNew) {
+            this.boardService.updateTasks(this.board.id, [
+              ...this.board.tasks,
+              result.task,
+            ]);
+          } else {
+            const update = this.board.tasks;
+            update.splice(result.idx, 1, result.task);
+            this.boardService.updateTasks(this.board.id, this.board.tasks);
+          }
+        }
+      }
+    });
+  }
+  handleDelete() {
+    if (!this.board.id) return;
+    this.boardService.deleteBoard(this.board.id);
   }
 }
